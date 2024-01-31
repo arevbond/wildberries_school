0e83d8dd7f8bc105e587ae7900380e53e53ea56b
arevbond <bondarev.n17@yandex.ru>
2024-01-31 16:26:04 +0300

    Done 21 task (l1)

--- second_task_l1/tasks/task_21/main.go
+++ second_task_l1/tasks/task_21/main.go
@@ -3,3 +3,41 @@
 */
 
 package main
+
+import "fmt"
+
+// интерфейс классов адаптера
+type Target interface {
+	Operation()
+}
+
+// адаптируемый класс
+type Adaptee struct {
+}
+
+// Метод адаптируемого класса, который нужно вызвать где-то
+func (adaptee *Adaptee) AdaptedOperation() {
+	fmt.Println("I am AdaptedOperation()")
+}
+
+// класс конкретного адаптера
+type ConcreteAdapter struct {
+	*Adaptee
+}
+
+// реализация метода интерфейса, реализующего вызов адаптируемого класса
+func (adapter *ConcreteAdapter) Operation() {
+	adapter.AdaptedOperation()
+}
+
+// конструктор адаптера
+func NewAdapter(adaptee *Adaptee) Target {
+	return &ConcreteAdapter{adaptee}
+}
+
+// основной метод для демонстрации
+func main() {
+	fmt.Println("\nAdapter demo:\n")
+	adapter := NewAdapter(&Adaptee{})
+	adapter.Operation()
+}
